9899d4d802cb4ffd1d808a91b094b8e41bb6ef45
Kutman <kutman2002mukarapov@gmail.com>
2024-12-28 00:58:13 +0600

    added cors origin

--- src/main/java/cam/onlinelibrarymanagementsystem/config/controller/AuthenticationController.java
+++ src/main/java/cam/onlinelibrarymanagementsystem/config/controller/AuthenticationController.java
@@ -4,10 +4,12 @@ import cam.onlinelibrarymanagementsystem.model.AuthenticationResponce;
 import cam.onlinelibrarymanagementsystem.model.User;
 import cam.onlinelibrarymanagementsystem.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class AuthenticationController {
 
--- src/main/java/cam/onlinelibrarymanagementsystem/config/controller/DemoController.java
+++ src/main/java/cam/onlinelibrarymanagementsystem/config/controller/DemoController.java
@@ -1,9 +1,11 @@
 package cam.onlinelibrarymanagementsystem.config.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class DemoController {
 
