e8c936f9c1ee418d8f1e2162e9ddc90a34b6a795
Kutman <83090460+ktmn21@users.noreply.github.com>
2024-12-30 12:29:50 +0600

    added the role so when registering ir returns the the role as well as token

--- src/main/java/cam/onlinelibrarymanagementsystem/model/AuthenticationResponce.java
+++ src/main/java/cam/onlinelibrarymanagementsystem/model/AuthenticationResponce.java
@@ -2,12 +2,22 @@ package cam.onlinelibrarymanagementsystem.model;
 
 public class AuthenticationResponce {
     private String token;
+    private String role;
 
+    public AuthenticationResponce(String token, String role) {
+        this.token = token;
+        this.role = role;
+    }
+    
     public AuthenticationResponce(String token){
         this.token = token;
     }
 
     public String getToken() {
         return token;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
